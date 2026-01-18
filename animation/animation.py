79ee65bf50d27fbed15275e5b62bf1e3dc55b0f6
Sharko3048 <amaya.arora@gmail.com>
2026-01-18 10:58:10 +0100

    Update animation.py

--- animation/animation.py
+++ animation/animation.py
@@ -6,17 +6,36 @@ from tkinter import *
 WIDTH=1920
 HEIGHT=1080
 
-bg=cv2.imread("/Users/Arora/Desktop/AOA-Jetlearn projects/opencv/images_cuz_yas/sukuna.jpg",cv2.IMREAD_COLOR)
+frames={"frame1":{
+    "bubble1":(710,750),
+    "bubble2":(1200,330),
+    "text1":(600,730),
+    "text2":(1050,320),
+    "char1":(450,750),
+    "char2":(1500,750)
+},
+"frame2":{
+    "bubble1":(710,330),
+    "bubble2":(1200,750),
+    "text1":(600,320),
+    "text2":(1050,730),
+    "char1":(450,750),
+    "char2":(1500,750)
+}}
 
-bg=cv2.resize(bg,(WIDTH,HEIGHT))
+spb_triangle={"top-left":[-105,-60,-105,-100,-75,-70],
+        "top-right":[105,-60,105,-100,75,-70],
+        "bottom-left":[-105,60,-105,100,-75,70],
+        "bottom-right":[105,60,105,100,75,70]}
 
 #drawing speech bubbles
-def speech_bubbles(img,pos):
+def speech_bubbles(img,pos,triangle_pos):
     x,y=pos
+    tp=spb_triangle[triangle_pos]
 
-    cv2.ellipse(img,pos,(190,80),0,0,360,(255,255,255),4)
-    coords=np.array([[[x-105,y-60],[x-105,y-100],[x-75,y-70]]])
-    cv2.polylines(img,coords,False,(255,255,255),4)
+    cv2.ellipse(img,pos,(190,80),0,0,360,(0,0,0),4)
+    coords=np.array([[[x+tp[0],y+tp[1]],[x+tp[2],y+tp[3]],[x+tp[4],y+tp[5]]]])
+    cv2.polylines(img,coords,False,(0,0,0),4)
 
 def multiline_text(img,pos,text):
     x,y=pos
@@ -36,11 +55,36 @@ def multiline_text(img,pos,text):
         cv2.putText(img,line,(x,y),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),4)
         y+=linegap
 
-speech_bubbles(bg,(720,750))
-multiline_text(bg,(600,730),"Hello I am not a human being")
+CHAR_WIDTH=300
+CHAR_HEIGHT=300
+
+def overlay_image(bg,char,pos):
+    x,y=pos
+    b,g,r,a=cv2.split(char)
+    alpha=(a/255)
+    for i in range(3):
+        bg[y:y+CHAR_HEIGHT,x:x+CHAR_WIDTH,i]=char[:,:,i]*alpha + (1-alpha)*bg[y:y+CHAR_HEIGHT,x:x+CHAR_WIDTH,i]
+    return bg
+
+bg=cv2.imread("/Users/Arora/Desktop/AOA-Jetlearn projects/opencv/animation/images_4_animation/white.png",cv2.IMREAD_COLOR)
+bg=cv2.resize(bg,(WIDTH,HEIGHT))
 
 
-cv2.imshow("test,",bg)
+
+char2=cv2.imread("/Users/Arora/Desktop/AOA-Jetlearn projects/opencv/animation/images_4_animation/Barry_Buns_S2.webp",cv2.IMREAD_UNCHANGED)
+char1=cv2.imread("/Users/Arora/Desktop/AOA-Jetlearn projects/opencv/animation/images_4_animation/kiff.png",cv2.IMREAD_UNCHANGED)
+char1=cv2.resize(char1,(CHAR_WIDTH,CHAR_HEIGHT))
+char2=cv2.resize(char2,(CHAR_WIDTH,CHAR_HEIGHT))
+panel=overlay_image(bg,char1,frames["frame1"]["char1"])
+panel=overlay_image(panel,char2,frames["frame1"]["char2"])
+
+
+speech_bubbles(panel,frames["frame1"]["bubble1"],"top-left")
+multiline_text(panel,frames["frame1"]["text1"],"Hello I am not a human being")
+speech_bubbles(panel,frames["frame1"]["bubble2"],"bottom-right")
+multiline_text(panel,frames["frame1"]["text2"],"yaasssssssssssssssss")
+
+
+cv2.imshow("test,",panel)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-    
