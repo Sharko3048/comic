f9ded87ac776e36515d71d7291c98ed49c7958ec
Sharko3048 <amaya.arora@gmail.com>
2026-03-28 13:56:15 +0100

    Add files via upload

--- animation/animation.py
+++ animation/animation.py
@@ -13,15 +13,16 @@ frames={"frame1":{
     "text2":(1405,695),
     "char1":(450,750),
     "char2":(1500,750),
-    #"char3":(1200,730)
+    "char3":(1200,730)
 },
 "frame2":{
     "bubble1":(710,330),
     "bubble2":(1200,750),
     "text1":(600,320),
     "text2":(1050,730),
-    "char1":(450,750),
-    "char2":(1500,750)
+    "char1":(450,450),
+    "char2":(1200,350),
+    "char3":(700,750)
 }}
 
 spb_triangle={"top-left":[-105,-60,-105,-100,-75,-70],
@@ -34,6 +35,7 @@ def speech_bubbles(img,pos,triangle_pos):
     x,y=pos
     tp=spb_triangle[triangle_pos]
 
+    cv2.ellipse(img,pos,(190,80),0,0,360,(255,255,255),-1)
     cv2.ellipse(img,pos,(190,80),0,0,360,(0,0,0),4)
     coords=np.array([[[x+tp[0],y+tp[1]],[x+tp[2],y+tp[3]],[x+tp[4],y+tp[5]]]])
     cv2.polylines(img,coords,False,(0,0,0),4)
@@ -84,13 +86,15 @@ for line in file:
 
     char1=cv2.imread(filepath+imgfpath+info[2],cv2.IMREAD_UNCHANGED)
     char2=cv2.imread(filepath+imgfpath+info[5],cv2.IMREAD_UNCHANGED)
-    #char3=cv2.imread(filepath+imgfpath+info[8],cv2.IMREAD_UNCHANGED)
     char1=cv2.resize(char1,(CHAR_WIDTH,CHAR_HEIGHT))
     char2=cv2.resize(char2,(CHAR_WIDTH,CHAR_HEIGHT))
-    #char3=cv2.resize(char3,(CHAR_WIDTH,CHAR_HEIGHT))
     panel=overlay_image(bg,char1,frames[frame]["char1"])
     panel=overlay_image(panel,char2,frames[frame]["char2"])
-    #panel=overlay_image(panel,char3,frames[frame]["char3"])
+    
+    if len(info) > 8:
+        char3=cv2.imread(filepath+imgfpath+info[8],cv2.IMREAD_UNCHANGED)
+        char3=cv2.resize(char3,(CHAR_WIDTH,CHAR_HEIGHT))
+        panel=overlay_image(panel,char3,frames[frame]["char3"])
 
     speech_bubbles(panel,frames[frame]["bubble1"],info[4])
     multiline_text(panel,frames[frame]["text1"],info[3])
